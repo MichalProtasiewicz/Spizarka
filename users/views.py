bb62d4008bab23f7381c2d6cd13dc80137f61f60
Michał Protasiewicz <protez95@gmail.com>
2020-03-13 00:07:50 +0100

    Commit

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from .models import User
 from .serializers import UserSerializer
 from rest_framework import generics
 
+
 class UserListCreate(generics.ListCreateAPIView):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
