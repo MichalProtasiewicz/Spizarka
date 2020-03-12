445824813e51e6aab0caedb445ff74df90a841ca
Michał Protasiewicz <protez95@gmail.com>
2020-03-13 00:04:50 +0100

    Update models.py

--- users/models.py
+++ users/models.py
@@ -14,6 +14,26 @@ class User(models.Model):
 class Order(models.Model):
   id_user = models.ForeignKey(User, on_delete=models.CASCADE)
   id_user = models.ForeignKey(Book, on_delete=models.CASCADE)
-  data_order = 
-  data_receipt =
-  data_return =
+  data_order = models.DateField()
+  data_receipt = models.DateField()
+  data_return = models.DateField()
+
+class Category(models.Model):
+  name = models.CharField(max_length=100)
+
+class Book(models.Model):
+  id_user = models.ForeignKey(Categoty, on_delete=models.CASCADE)
+  isbn = models.CharField(max_length=100)
+  name = models.CharField(max_length=100)
+  author = models.CharField(max_length=100)
+  publishing = models.CharField(max_length=100)
+  data_publication = models.DateField()
+  description = models.CharField(max_length=100)
+
+class Employee(models.Model):
+  login = models.CharField(max_length=100, unique=True)
+  password = models.CharField(max_length=100)
+
+class Admin(models.Model):
+  login = models.CharField(max_length=100, unique=True)
+  password = models.CharField(max_length=100)
