bf839a48fb3625501faec28105e8f1215d269103
Michał Protasiewicz <protez95@gmail.com>
2020-04-15 09:51:03 +0200

    Update settings.py

--- StarMovie/StarMovie/settings.py
+++ StarMovie/StarMovie/settings.py
@@ -123,6 +123,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
 
-
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
