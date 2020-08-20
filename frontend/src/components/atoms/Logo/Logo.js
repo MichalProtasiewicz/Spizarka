006bfedcb1dd5e877f42d15d13a4603cbb508119
Michał Protasiewicz <protez95@gmail.com>
2020-08-20 14:37:37 +0200

    Update Logo.js

--- frontend/src/components/atoms/Logo/Logo.js
+++ frontend/src/components/atoms/Logo/Logo.js
@@ -4,6 +4,10 @@ import { NavLink } from 'react-router-dom';
 import { routes } from 'routes';
 import logoIcon from 'assets/logo.svg';
 
+const StyledLogoWrapper = styled.div`
+  display: flex;
+`;
+
 const StyledLogoLink = styled(NavLink)`
   display: block;
   width: 67px;
@@ -15,10 +19,25 @@ const StyledLogoLink = styled(NavLink)`
   border: none;
 `;
 
+const StyledLogoName = styled.h1`
+  margin: 18px 0 0 10px;
+  font-size: ${({ theme }) => theme.fontSize.l};
+  font-weight: ${({ theme }) => theme.bold};
+  color: ${({ theme }) => theme.white};
+
+  @media (min-width: 800px) {
+    display: none;
+  }
+  @media (max-width: 300px) {
+    display: none;
+  }
+`;
+
 const Logo = () => (
-  <>
+  <StyledLogoWrapper>
     <StyledLogoLink to={routes.home} />
-  </>
+    <StyledLogoName>Spizarka</StyledLogoName>
+  </StyledLogoWrapper>
 );
 
 export default Logo;
